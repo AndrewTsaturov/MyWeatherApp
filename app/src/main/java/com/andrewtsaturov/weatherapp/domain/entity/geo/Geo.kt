ecd351af54985f6e1ac228156c3d6d0051464ce4
android <andrewtsaturov@gmail.com>
2021-02-17 13:53:12 +0300

    intermediate

--- app/src/main/java/com/andrewtsaturov/weatherapp/domain/entity/geo/Geo.kt
+++ app/src/main/java/com/andrewtsaturov/weatherapp/domain/entity/geo/Geo.kt
@@ -2,7 +2,7 @@ package com.andrewtsaturov.weatherapp.domain.entity.geo
 
 import androidx.room.Entity
 
-@Entity
+@Entity(primaryKeys = arrayOf("latitude", "longitude"))
 data class Geo(
     val name: String,
     val title: String,
